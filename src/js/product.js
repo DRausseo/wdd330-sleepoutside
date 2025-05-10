714997f63f8ef5c9cc2153cf58c398ff61bf7d1a
DRausseo <dannymrleon@yahoo.com>
2025-05-10 01:05:11 -0300

    Fix add to cart functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // 1. Obtener el carrito actual
+  let cart = getLocalStorage("so-cart") || [];
+
+  // 2. Agregar el nuevo producto
+  cart.push(product);
+
+  // 3. Guardar el carrito actualizado
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
